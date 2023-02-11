3a7f88bd5509fc8633e9cc6b669f2f5284d1a378
CHANDRA MOHAN <thechandramohan01@gmail.com>
2023-02-11 19:29:06 +0530

    Restructuring the project

--- routes/customers.js
+++ routes/customers.js
@@ -1,34 +1,15 @@
-const Joi = require('joi')
+const {Customer, validate} = require('../models/customer')
 const mongoose = require('mongoose')
 const express = require('express');
 const router = express.Router();
-
-const Customer = mongoose.model('Customer', new mongoose.Schema({
-    name: {
-        type:String,
-        required:true,
-        minlength:5,
-        maxlength:50
-    },
-    isGold:{
-        type: Boolean,
-        default: false
-    },
-    phone: {
-        type:String,
-        required:true,
-        minlength:5,
-        maxlength:50
-    }
-}))
-  
+ 
 router.get('/', async(req, res) => {
     const customers = await Customer.find().sort('name')
     res.send(customers);
 });
 
 router.post('/', async(req, res) => {
-    const { error } = validateCustomer(req.body); 
+    const { error } = validate(req.body); 
     if (error) return res.status(400).send(error.details[0].message);
   
     let customer = new Customer({
@@ -42,7 +23,7 @@ router.post('/', async(req, res) => {
 });
 
 router.put('/:id', async(req, res) => {
-    const { error } = validateCustomer(req.body); 
+    const { error } = validate(req.body); 
     if (error) return res.status(400).send(error.details[0].message);
     
     const customer = await Customer.findByIdAndUpdate(req.params.id, { 
@@ -59,7 +40,7 @@ router.put('/:id', async(req, res) => {
 router.delete('/:id', async(req, res) => {
     const customer = await Customer.findByIdAndRemove(req.params.id)
 
-    if (!customer) return res.status(404).send('The genre with the given ID was not found.');
+    if (!customer) return res.status(404).send('The customer with the given ID was not found.');
 
     res.send(customer);
 });
@@ -71,15 +52,5 @@ router.get('/:id', async(req, res) => {
     
     res.send(customer);
 });
-
-function validateCustomer(customer) {
-    const schema = {
-      name: Joi.string().min(5).max(50).required(),
-      phone: Joi.string().min(5).max(15).required(),
-      isGold: Joi.boolean()
-    };
-  
-    return Joi.validate(customer, schema);
-};
 
 module.exports = router
